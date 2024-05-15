1b7bdebf9875ca8b3b1f7525e45225cbb62f2e73
killthmxall <killthmxall@outlook.com>
2024-05-15 00:13:35 -0500

    Se agrega el ejercicio 1.1 y ejercicio 1.2

--- part1/ejercicio1.2/src/App.jsx
+++ part1/ejercicio1.2/src/App.jsx
@@ -8,20 +8,24 @@ const Header = (props) => {
   )
 }
 
+//Componente Part
+const Part = (props) => {
+  console.log(props)
+  return(
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
 // Componente Content
 const Content = (props) => {
   console.log(props)
   return(
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part = {props.part1} exercises = {props.exercises1}/>
+      <Part part = {props.part2} exercises = {props.exercises2}/>
+      <Part part = {props.part3} exercises = {props.exercises3}/>
     </div>
   )
 }
@@ -45,21 +49,6 @@ const App = () => {
   const exercises3 = 14
 
   return (
-    /*
-    <div>
-      <h1>{course}</h1>
-        <p>
-          {part1} {exercises1}
-        </p>
-        <p>
-          {part2} {exercises2}
-        </p>
-        <p>
-          {part3} {exercises3}
-        </p>
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
-    */
     <div>
       <Header course = {course}/>
       <Content part1 = {part1} exercises1 = {exercises1} 
@@ -71,9 +60,5 @@ const App = () => {
   )
 
 }
-
-
-
-
 export default App
 
